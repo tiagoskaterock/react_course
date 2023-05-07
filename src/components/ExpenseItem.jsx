a3f934198e0a62db42b64a4a0b63554db067c330
Mary Cupcakes <tiagolemespalhano@gmail.com>
2023-05-06 22:28:21 -0300

    Extensions changed to .jsx

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,11 +1,11 @@
-import ExpenseDate from './ExpenseDate'
-
+import ExpenseDate from './ExpenseDate.jsx'
+import Card from './Card.jsx'
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
 
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
 
             <ExpenseDate date={ props.date } />
             
@@ -13,7 +13,7 @@ function ExpenseItem(props) {
                 <h2>{ props.title }</h2>
                 <div className="expense-item__price">${ props.amount }</div>
             </div>            
-        </div>
+        </Card>
     )
 }
 
--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,4 +1,5 @@
-import ExpenseItem from './ExpenseItem'
+import ExpenseItem from './ExpenseItem.jsx'
+import Card from './Card.jsx'
 import './Expenses.css'
 
 function Expenses(props) {
@@ -8,7 +9,7 @@ function Expenses(props) {
 
 			<h2>Let's get started!</h2>
 
-			<div className="expenses">
+			<Card className="expenses">
 				<ExpenseItem 
 					title={props.items[0].title} 
 					amount={props.items[0].amount} 
@@ -32,7 +33,7 @@ function Expenses(props) {
 					amount={props.items[3].amount} 
 					date={props.items[3].date} 
 				/>						
-			</div>
+			</Card>
 
 
 		</div> 
